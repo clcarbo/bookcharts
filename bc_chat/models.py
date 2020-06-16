00918213108505bb1d91d210c8c0bc7a7aac7012
clcarbo <clcarbo1st@gmail.com>
2020-06-15 23:21:47 -0400

    Slowly

--- bc_chat/models.py
+++ bc_chat/models.py
@@ -1,20 +1,21 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
 
-class User(models.Model):
-    username = models.CharField(max_length=20)
-    passhash = models.CharField(max_length=50)
-    email = models.CharField(max_length=150)
-    date_joined = models.DateTimeField('date joined')
+# class Book(models.Model):
+#     
+# 
+# class Chart(models.Model):
 
-    def __str__(self):
-        return self.username
 
 class Comment(models.Model):
     text = models.CharField(max_length=200)
-    poster_id = models.ForeignKey(User, models.PROTECT)
     post_date = models.DateTimeField('date posted')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE,
+                             default="")
 
     def __str__(self):
         return self.text
